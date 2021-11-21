d396be8e6d6fbca8e4d33320e782b2ac46b538ff
hyunsooryu <41038031+hyunsooryu@users.noreply.github.com>
2021-11-21 16:02:02 +0900

    spring-boot-data-study
    2021-11-21
    spring-boot-data 공부

---
+++ src/main/java/me/hyunsoo/artofspringbootdata/ArtOfSpringBootDataApplication.java
@@ -0,0 +1,13 @@
+package me.hyunsoo.artofspringbootdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtOfSpringBootDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtOfSpringBootDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hyunsoo/artofspringbootdata/ArtOfSpringBootDataApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyunsoo.artofspringbootdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtOfSpringBootDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
